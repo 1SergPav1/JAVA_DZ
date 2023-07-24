9117307711a9b93f3dc18a5cb0b541b63962255c
1SergPav1 <pav220521@gmail.com>
2023-07-24 10:54:35 +0300

    OOP Seminar2 DZ_2

---
+++ src/main/java/Seminar_2/DZ_2.java
@@ -0,0 +1,18 @@
+package Seminar_2;
+
+public class DZ_2 {
+
+    public static void main(String[] args) {
+
+        Market market = new Market();
+        Human human1 = new Human("Дима");
+        Human human2 = new Human("Света");
+
+        market.acceptToMarket(human2);
+        market.acceptToMarket(human1);
+        market.update();
+
+    }
+
+
+}
